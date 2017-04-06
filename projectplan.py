210ece2968b47867192b5322b3b22fe5228841a6
Sara Ramaswamy <sararama@umich.edu>
2017-04-06 08:32:51 -0400

    Adding project files

---
+++ projectplan.py
@@ -0,0 +1,33 @@
+## Your name:
+## The option you've chosen: Option 2
+
+# Put import statements you expect to need here!
+# import unittest  # for testing
+# import itertools ## for generators and list comprehension
+# import collections ## for containers and Counter
+# import tweepy ## for using the twitter api
+# import twitter_info  ## for connecting to my personal key
+# import json ## for downloading api and formating api data for python manipulation
+# import sqlite3 ## for creating and querying my 3 Tables (Movies, Tweets, Users)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Write your test cases here.
+
+
+
+## Remember to invoke all your tests...
