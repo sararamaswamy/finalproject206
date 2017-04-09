aa8817993c576365a9d6e1e265f4c0e4ef40779b
Sara Ramaswamy <sararama@umich.edu>
2017-04-08 21:31:06 -0400

    added the import statement for omdb after reading documentation

--- projectplan.py
+++ projectplan.py
@@ -2,15 +2,17 @@
 ## The option you've chosen: Option 2
 
 # Put import statements you expect to need here!
-# import unittest  ## for testing
-# import itertools ## for generators and list comprehension
-# import collections ## for containers and Counter
-# import tweepy ## for using the twitter api
-# import twitter_info  ## for connecting to my personal key
-# import json ## for downloading api and formating api data for python manipulation
-# import sqlite3 ## for creating and querying my 3 Tables (Movies, Tweets, Users)
+import unittest  ## for testing
+import itertools ## for generators and list comprehension
+import collections ## for containers and Counter
+import tweepy ## for using the twitter api
+import twitter_info  ## for connecting to my personal key
+import json ## for downloading api and formating api data for python manipulation
+import sqlite3 ## for creating and querying my 3 Tables (Movies, Tweets, Users)
+import omdb ## After I pip install omdb, i will be able to make requests to the omdb api
 
 ## Pseudocode for functions begins here
+
 
 #def get_omdb_data(movie_title):
 		##accepts a string movie title
